1dde6defb21c6a210934ddfa96acbe8c0fc45800
David Ross Smith <5095074+DragRedSim@users.noreply.github.com>
2024-02-10 00:56:26 +1100

    Take two...

--- check-clrmamepro.py
+++ check-clrmamepro.py
@@ -9,6 +9,5 @@ def get_version():
     parser.read_string(homepage.text)
     return parser['clrmamepro']['version']
 
-env_file = os.getenv('GITHUB_ENV')
-with open(env_file, "a") as env:
-    env.write(f"LATEST_VERSION={get_version()}\n")
+with open(os.environ['GITHUB_OUTPUT'], "a") as env:
+    print(f"LATEST_VERSION={get_version()}\n", file=env)
